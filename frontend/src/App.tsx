a7f1a05699f20cc767899af3ae3e56afef5c015d
mario1980miranda <mario_chan15@hotmail.com>
2022-08-07 09:33:48 -0400

    frontend initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Header</h1>
+      <h2>Select Filter Card</h2>
+      <h2>Total sales and chart</h2>
+    </div>
+  );
+}
+
+export default App;
