8ae1ba4060a67d6c040a3a00a49b2d5028eb7c25
mario1980miranda <mario_chan15@hotmail.com>
2022-08-08 10:19:00 -0400

    sales summary html basics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import Header from './components/Header';
+import SelectComponent from './components/SelectComponent';
+import SalesSummary from './components/SalesSummary';
 
 import './App.css';
-import SelectComponent from './components/SelectComponent';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <div className="app-container">
         <SelectComponent />
-        <h2>Total sales and chart</h2>
+        <SalesSummary />
       </div>
     </div>
   );
---
+++ frontend/src/components/SalesSummary/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+function SalesSummary() {
+  return (
+    <div className="sales-summary-card base-card">
+      <div className="sales-summary-top">
+        <h1>R$ 746.484,00</h1>
+        <span>Total de vendas</span>
+      </div>
+      <div className="sales-summary-bottom">Chart</div>
+    </div>
+  );
+}
+
+export default SalesSummary;
